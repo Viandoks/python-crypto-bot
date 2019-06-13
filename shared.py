4b65f8268fb3db419942de66993f00c57d392c8c
Alex <alex@gokunming.com>
2019-06-13 16:40:19 +0800

    shared.py issue

---
+++ shared.py
@@ -0,0 +1,19 @@
+api = {
+    'key': '',
+    'secret': ''
+}
+
+strategy = {
+    'movingAverageLength': 14
+}
+
+exchange = {
+    'name': 'poloniex',
+    'spreadPercentage': 0.002,
+    'fees': 0.002,
+    'pair': None,
+    'market': None,
+    'nbMarket': 1,
+    'coin': None,
+    'nbCoin': 1
+}
